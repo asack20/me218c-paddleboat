a63eeca2d581cd421e90c786dafb736cb65b9b96
Andrew Sack <agsack20@gmail.com>
2022-02-26 14:21:17 -0800

    Changed params for beacon rotate

--- DrivePic/DriveTrain/DriveTrain.c
+++ DrivePic/DriveTrain/DriveTrain.c
@@ -37,7 +37,7 @@
 // Command Tuning
 #define DRIVE_DEBUG
 
-#define SWEEP_CW_ANGLE 10
+#define SWEEP_CW_ANGLE 30
 
 /*----------------------------- Module Types ------------------------------*/
 /*---------------------------- Module Functions ---------------------------*/
@@ -229,7 +229,7 @@ ES_Event_t RunDriveTrain(ES_Event_t ThisEvent)
                 case DRIVE_BEACON_SWEEP:
                 {
                     // Turn Clockwise by SWEEP_CW_ANGLE at slow speed
-                    MotorControl_DriveTurn(_Clockwise_Turn, Speeds[Medium], SWEEP_CW_ANGLE);
+                    MotorControl_DriveTurn(_Clockwise_Turn, Speeds[Low], SWEEP_CW_ANGLE);
 #ifdef DRIVE_DEBUG
                     printf("DriveDebug: Starting Drive Clockwise Sweep\r\n");
 #endif
@@ -316,7 +316,7 @@ ES_Event_t RunDriveTrain(ES_Event_t ThisEvent)
                 SweepAmount = LeftEncoder.TickCount;
                 
                 // Turn CounterClockwise by 360 + SWEEP_CW_ANGLE at slow speed
-                MotorControl_DriveTurn(_CounterClockwise_Turn, Speeds[Medium], 360+SWEEP_CW_ANGLE);
+                MotorControl_DriveTurn(_CounterClockwise_Turn, Speeds[Low], 360+SWEEP_CW_ANGLE);
                 
                 CurrentState = DriveCounterClockwiseSweepState;
 #ifdef DRIVE_DEBUG
